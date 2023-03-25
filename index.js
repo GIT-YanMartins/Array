cc16bc446aa13782293a03eb651a440f7ff4462e
GIT-YanMartins <yangabriel15@outlook.com>
2023-03-25 16:48:13 -0300

    ainda em andamento

---
+++ index.js
@@ -0,0 +1,94 @@
+//quest I
+let questI = ['maça', 'banana', 'uva', 'whey', 'morango'];
+console.log (questI);
+
+//quest II
+let questII = ['maça', 'banana', 'uva', 'whey', 'creatina'];
+questII.unshift('laranja')
+console.log (questII);
+
+//questi III
+let questIII = ['maça', 'banana', 'uva', 'whey', 'maca']
+let Sevenitens = questIII.pop()
+console.log(questIII)
+
+//quest IV
+let questIV = ['maça', 'cana', 'uva', 'whey', 'creatina'];
+questIV.push('abacaxi', 'melancia')
+console.log(questIV)
+
+//quest V
+let questV = ['maça', 'çimao', 'uva', 'whey', 'creatina'];
+questV.push('abacaxi')
+console.log(questV)
+
+//quest VI
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log (numbers.sort())
+
+//quest VII
+let props = [
+    {
+        propriedade1: 'calmo',
+        propriedade2: 'disciplinado',
+        propriedade3: 'metódico,'
+    }
+]
+console.log (props)
+
+//quest VII (adicionando nova propriedade)
+let props2 = [
+    {
+        propriedade1: 'calmo',
+        propriedade2: 'disciplinado',
+        propriedade3: 'metódico,'
+    }
+]
+props2.push ({
+    propriedade4: 'cabeça fria'
+})
+console.log(props2)
+
+//questVIII, IX, XI e XII
+//não consegui fazer, por favor, comenta me ensinando?
+
+//quest XI
+let cadastro = [
+
+    {
+        nome: 'yan',
+        idade: '0',
+        telefone: '',
+        amigos: 'joao, Luis, mairon, vinicius, vitoria'
+    
+},
+{
+    nome: 'yan',
+    idade: '0',
+    telefone: '',
+    amigos: 'joao, Henrique, mairon, vinicius, vitoria'
+
+},
+{
+    nome: 'yan',
+    idade: '0',
+    telefone: '',
+    amigos: 'joao, Milena, mairon, vinicius, vitoria'
+
+},
+{
+    nome: 'yan',
+    idade: '0',
+    telefone: '',
+    amigos: 'joao, Geane, mairon, vinicius, vitoria'
+
+},
+{
+    nome: 'yan',
+    amigos: 'joao, naomi, mairon, vinicius, vitoria'
+
+},
+
+]
+console.log (cadastro)
+
